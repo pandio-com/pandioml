a4ef90433225d3293717e47b8b2b627f8834a085
odmarkj <josh@isell.com>
2021-05-10 08:28:54 -0700

    Adding a requirements file to support pandiocli packaging

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ pip uninstall pandioml -y
 
 python setup.py bdist_wheel
 
-pip install dist/pandioml-1.0.0-py3-none-any.whl
+pip install dist/pandioml-1.0.2-py3-none-any.whl
--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ this_directory = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
+with open(os.path.join(this_directory, 'requirements.txt')) as f:
+    required = f.read().splitlines()
+
 setuptools.setup(
     name="pandioml",
     version="1.0.2",
@@ -21,13 +24,7 @@ setuptools.setup(
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
     ],
-    # For Testing
-    # install_requires=['numpy==1.20.1', 'pulsar-client==2.6.0', 'Faker==8.1.1',
-    #                  'river @ git+https://github.com/pandio-com/river@0.7.0#egg=river'],
-    # For Release
-    install_requires=['numpy==1.20.1', 'pulsar-client==2.6.0', 'Faker==8.1.1', 'river==0.7.0', 'scikit-learn==0.24.1'],
-    # For PandioCLI
-    # install_requires=['numpy==1.20.1', 'Faker==8.1.1', 'river==0.7.0', 'scikit-learn==0.24.1'],
+    install_requires=required,
     setup_requires=['pytest-runner'],
     tests_require=['pytest==4.4.1'],
     test_suite='tests'
