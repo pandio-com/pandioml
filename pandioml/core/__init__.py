f84659754703d7ecd44e0e74dd109d23444bbea8
odmarkj <josh@isell.com>
2021-05-24 12:54:29 -0700

    Removing storage due to library errors

--- pandioml/core/__init__.py
+++ pandioml/core/__init__.py
@@ -1,10 +1,10 @@
 from .pipelines import Pipelines, Pipeline
 from .interact import interact
-from .storage import storage
+#from .storage import storage
 
 __all__ = [
     "Pipelines",
     "Pipeline",
     "interact",
-    "storage",
+    #"storage",
 ]
