07e453faf142c8b4e857f61962771fa3c49c4952
odmarkj <josh@isell.com>
2021-06-07 15:39:21 -0700

    Bumping pandiocli version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ EXTRAS_REQUIRE = {
 
 setuptools.setup(
     name="pandioml",
-    version="1.0.11",
+    version="1.0.12",
     author="Joshua Odmark",
     author_email="josh@pandio.com",
     description="Pandio's machine learning library.",
